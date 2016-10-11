899269d6efda8e6d74ea608fcc2b01a604467118
taylor <taylor.wan@fiscalnote.com>
2016-10-11 16:23:26 -0400

    fend against duplicates for at least 10 weeks

--- pair.py
+++ pair.py
@@ -1,6 +1,13 @@
 import csv
 import random
 
+path     = 'pairs/'
+ext      = '.txt'
+maxLines = 10
+
+## LOAD ##
+
+# load all users from a csv file
 def load_people(filename):
   with open(filename, 'rb') as csvfile:
     f = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -18,20 +25,99 @@ def load_people(filename):
       people.append(person)
   return people
 
-def pair(l):
+## PAIR ##
+
+# pair all users in a list
+def pairAll(l):
   pairings = []
+
   while len(l) > 1:
+    # choose random pairs
     a = random.choice(l)
     l.remove(a)
     b = random.choice(l)
     l.remove(b)
-    pairings.append(a['name'] + " and " + b['name'])
+
+    # if this pairing hasn't been matched recently
+    while hasPaired(a,b):
+      c = random.choice(l)
+      l.remove(c)
+      l.append(b)
+      b = c
+
+    # save and add the pair to our list
+    savePair(a, b)
+    pairings.append(a['name'] + ' and ' + b['name'])
+
   return pairings
 
+# check if two people have already been paired
+def hasPaired(a, b):
+  afile = getFileName(a)
+  return isInFile(afile, b['name'])
+
+# save each name in respective files
+def savePair(a, b):
+  afile = getFileName(a)
+  bfile = getFileName(b)
+
+  # write or append to respective files
+  if fileLongerThan(afile, maxLines):
+    writeFile(afile, b['name'] + '\n')
+  else:
+    appendToFile(afile, b['name'] + '\n')
+
+  if fileLongerThan(bfile, maxLines):
+    writeFile(bfile, a['name'] + '\n')
+  else:
+    appendToFile(bfile, a['name'] + '\n')
+
+
+## HELPERS ##
+
+# generate the file name for a particular user
+def getFileName(a):
+  return path + a['first'] + a['last'] + ext
+
+# check if a file is longer than a specified length n
+def fileLongerThan(filename, n):
+  print 'checking if', filename, 'is greater than', n, 'lines'
+  try:
+    lines = sum(1 for line in open(filename))
+    if lines > n:
+      return True  # more than N lines in the file
+    return False  # less than N lines in the file
+  except:
+    return False  # file not found
+
+# append s to a file
+def appendToFile(filename, s):
+  with open(filename, 'a') as f:
+    f.write(s)
+
+# write s to a file: overwrites existing file
+def writeFile(filename, s):
+  with open(filename, 'w') as f:
+    f.write(s)
+
+# check if s is in a file
+def isInFile(filename, s):
+  try:
+    with open(filename, 'r') as f:
+      data = f.read()
+      if s in data:
+        return True  # s is in data
+      return False  # is not in data
+  except:
+    return False  # file doesn't exist
+
+# print a list of items
 def printList(l):
   for v in l:
     print v
 
+
+## MAIN ##
 
 def main():
   # generate list of active people
@@ -40,7 +126,7 @@ def main():
 
   # generate list of active people
   print 'Pairing...'
-  pairs = pair(people)
+  pairs = pairAll(people)
 
   # print
   printList(pairs)
@@ -48,3 +134,4 @@ def main():
 
 if __name__ == "__main__":
   main()
+  # fileLongerThan()
